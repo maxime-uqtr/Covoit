cbea238a8246049e0ab4dcaf6e0795c1dbbe2e04
Marius Couet <marius@couet.net>
2020-03-23 22:54:11 +0100

    Remis animation + setclickable sur les FAB

--- app/src/main/java/com/exemple/Covoit/vue/MainActivity.java
+++ app/src/main/java/com/exemple/Covoit/vue/MainActivity.java
@@ -10,13 +10,13 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 
+import com.exemple.Covoit.BoutonAnimation;
 import com.exemple.Covoit.ListAdapteur;
 import com.exemple.Covoit.OnListClickListener;
 import com.exemple.Covoit.R;
 import com.exemple.Covoit.bd.CovoiturageBd;
 import com.exemple.Covoit.models.Covoiturage;
 import com.exemple.Covoit.models.Recherche;
-import com.exemple.Covoit.models.Utilisateur;
 import com.facebook.stetho.Stetho;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
@@ -31,6 +31,7 @@ public class MainActivity extends AppCompatActivity implements OnListClickListen
     private CovoiturageBd bd;
     private FloatingActionButton FABrechercheCovoiturage;
     private FloatingActionButton FABproposeCovoiturage;
+    private FloatingActionButton FABouvrir;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,13 +40,35 @@ public class MainActivity extends AppCompatActivity implements OnListClickListen
 
         Stetho.initializeWithDefaults(this); //Ajout de stetho à l'activité
 
+
+
         FABproposeCovoiturage = findViewById(R.id.main_FAB_propose);
+        BoutonAnimation.hide(FABproposeCovoiturage);
         FABrechercheCovoiturage = findViewById(R.id.main_FAB_recherche);
+        BoutonAnimation.hide(FABrechercheCovoiturage);
         FABproposeCovoiturage.setOnClickListener(v -> {
-            Intent rechercheIntent = new Intent(this, Recherche.class);
-            startActivity(rechercheIntent);
+                Intent rechercheIntent = new Intent(this, Recherche.class);
+                startActivity(rechercheIntent);
         });
 
+        FABouvrir = findViewById(R.id.main_FAB_ouvrir);
+        FABouvrir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                isRotate = BoutonAnimation.rotateFab(v, !isRotate);
+                if(isRotate){
+                    BoutonAnimation.show(FABproposeCovoiturage);
+                    FABproposeCovoiturage.setClickable(true);
+                    BoutonAnimation.show(FABrechercheCovoiturage);
+                    FABrechercheCovoiturage.setClickable(true);
+                }else{
+                    BoutonAnimation.hide(FABproposeCovoiturage);
+                    FABproposeCovoiturage.setClickable(false);
+                    BoutonAnimation.hide(FABrechercheCovoiturage);
+                    FABrechercheCovoiturage.setClickable(false);
+                }
+            }
+        });
 
         bd = CovoiturageBd.getInstance(getApplicationContext());
         Covoiturage c = new Covoiturage(1, new Date(2020, 06,13),
