111f0c652e24d98f31e27eb16bee4333bf17c238
Marius Couet <marius@couet.net>
2020-03-24 01:00:30 +0100

    mise en place de controleur connection & inscription

---
+++ app/src/main/java/com/exemple/Covoit/controleur/Connection.java
@@ -0,0 +1,8 @@
+package com.exemple.Covoit.controleur;
+
+public class Connection {
+    public boolean Connection(String mail, String mdp){
+
+        return true;
+    }
+}
--- app/src/main/java/com/exemple/Covoit/vue/Inscription.java
+++ app/src/main/java/com/exemple/Covoit/vue/Inscription.java
@@ -1,4 +1,16 @@
 package com.exemple.Covoit.vue;
 
-public class Inscription {
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import com.exemple.Covoit.R;
+
+public class Inscription extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inscription);
+
+    }
 }
